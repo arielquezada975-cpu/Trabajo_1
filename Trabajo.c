d82dfcda81c24431567306b7f028946f4abeee20
MrGato <arielquezada975@gmail.com>
2026-08-20 13:06:03 -0500

    parte 1

---
+++ Trabajo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+    int inicio, fin, incremento;
+ 
+    // Solicitar datos al usuario
+    printf("=== Generador de Secuencia Numerica ===\n\n");
+ 
+    printf("Ingrese el numero inicial:    ");
+    scanf("%d", &inicio);
+ 
+    printf("Ingrese el numero final:      ");
+    scanf("%d", &fin);
+ 
+    printf("Ingrese el valor de incremento: ");
+    scanf("%d", &incremento);
+
+    
+    return 0;
+}
